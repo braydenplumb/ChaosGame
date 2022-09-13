e786fbd394666261cfde99288065b12ed063746b
Brayden Plumb <braydenplumb@gmail.com>
2022-09-13 00:21:23 -0700

    SFML Setup and Working!

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-using namespace std;
+using namespace sf;
 
 int main()
 {
-	return 0;
+    RenderWindow window(VideoMode(1280, 720), "Chaos Game");
+
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+
+    return 0;
 }
