9a60c6c72152f71472a8167e30d12d881ee59032
BRAYDEN PLUMB <braydenplumb@gmail.com>
2022-09-13 16:05:27 -0700

    test

--- main.cpp
+++ main.cpp
@@ -9,22 +9,40 @@ int main()
 {
     RenderWindow window(VideoMode(1280, 720), "Chaos Game");
 
+    Color darkGray(50, 50, 50, 150);
     RectangleShape rect;
-    rect.setSize(Vector2f(340, 200));
+    rect.setSize(Vector2f(720, 150));
     rect.setPosition(0, 0);
+    rect.setFillColor(darkGray);
 
     Font mainFont;
     mainFont.loadFromFile("fonts/VT323-Regular.ttf");
 
+    Text titleText;
+    titleText.setFont(mainFont);
+    titleText.setString("Welcome to the Chaos Game!");
+    titleText.setCharacterSize(30);
+    titleText.setColor(Color::White);
+    titleText.setPosition(Vector2f(200, 10));
+
     Text instructionText;
     instructionText.setFont(mainFont);
-    instructionText.setString("Welcome to the Chaos Game!");
-    instructionText.setCharacterSize(30);
-    instructionText.setColor(Color::Black);
-    instructionText.setPosition(Vector2f(10, 10));
+    instructionText.setString("Step 1: Click on any three points on the screen to create the vertices for the triangle\nStep 2: Click on a fourth point to start the algorithm\nStep 3: Chaos Ensues!\nPress Enter to Close Instructions");
+    instructionText.setCharacterSize(20);
+    instructionText.setColor(Color::White);
+    instructionText.setPosition(Vector2f(10, 44));
     bool showText = true;
 
     vector<Vector2f> vertices;
+
+    // Generate Random Points - for testing
+    srand(time(NULL));
+    for (int i = 0; i < 400; i++)
+    {
+        int rand_x = rand() % 1280;
+        int rand_y = rand() % 720;
+        vertices.push_back(Vector2f(rand_x, rand_y));
+    }
 
     while (window.isOpen())
     {
@@ -62,9 +80,19 @@ int main()
 
         window.clear();
 
+        // Draws the Points
+        for (int i = 0; i < vertices.size(); i++)
+        {
+            RectangleShape rectPoint;
+            rectPoint.setSize(Vector2f(1, 1));
+            rectPoint.setPosition(vertices.at(i));
+            window.draw(rectPoint);
+        }
+
         if (showText)
         {
             window.draw(rect);
+            window.draw(titleText);
             window.draw(instructionText);
         }
 
