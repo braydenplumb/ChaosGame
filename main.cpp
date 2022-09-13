c8d2746e5ebb1fc889219a27f314e8312514688d
BRAYDEN PLUMB <braydenplumb@gmail.com>
2022-09-13 14:25:50 -0700

    Rectangle Rendering Stuff

--- main.cpp
+++ main.cpp
@@ -6,6 +6,15 @@ int main()
 {
     RenderWindow window(VideoMode(1280, 720), "Chaos Game");
 
+    RectangleShape rect;
+    rect.setSize(Vector2f(200, 200));
+    rect.setPosition(100, 100);
+
+    RectangleShape rect_2;
+    rect_2.setSize(Vector2f(300, 300));
+    rect_2.setPosition(150, 150);
+    rect_2.setFillColor(Color::Green);
+
     while (window.isOpen())
     {
         /*
@@ -28,6 +37,10 @@ int main()
         */
 
         window.clear();
+        
+        window.draw(rect_2);
+        window.draw(rect);
+
         window.display();
     }
 
